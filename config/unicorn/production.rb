bdf37747b369c45c4a8c9b2b64a715c6bbb59faa
Gerardo Ortega <geraldavid7@gmail.com>
2014-07-06 10:51:01 -0600

    New conf for unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,10 +1,10 @@
-root = "/root/atmapp/current"
+root = "/home/gerardo/atmapp/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.atmapp.sock"
+listen "/home/gerardo/tmp/unicorn.atmapp.sock"
 worker_processes 2
 timeout 30
 
